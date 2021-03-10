2c2b9be88677358b0c72d4145ec303d5af98065b
natamaz <sidorova87@mail.ru>
2021-03-10 19:59:21 +0200

    index

---
+++ assets/js/home.js
@@ -0,0 +1,59 @@
+
+
+$(document).ready(function () {
+    <!-- Slider-->
+    //$('.specialist_slider').slick({
+    //    draggable:true,
+    //    slidesToShow: 3,
+    //    slidesToScroll: 1,
+    //    infinite:true,
+    //    dots: true,
+    //    arrows:false,
+    //    responsive: [
+    //
+    //        {
+    //
+    //            breakpoint: 1280,
+    //            settings: {
+    //                slidesToShow: 2,
+    //                slidesToScroll: 1,
+    //            }
+    //        },
+    //        {
+    //
+    //            breakpoint: 1024,
+    //            settings: {
+    //                slidesToShow: 2,
+    //                slidesToScroll: 1,
+    //            }
+    //        },
+    //        {
+    //            breakpoint: 768,
+    //            settings: {
+    //                slidesToShow: 1,
+    //                slidesToScroll: 1,
+    //            }
+    //        },
+    //        {
+    //            breakpoint: 480,
+    //            settings: {
+    //                slidesToShow: 1,
+    //                slidesToScroll: 1,
+    //            }
+    //        },
+    //        {
+    //            breakpoint: 320,
+    //            settings: {
+    //                slidesToShow: 1,
+    //                slidesToScroll: 1,
+    //            }
+    //        },
+    //
+    //    ]
+    //});
+    <!-- Slider and -->
+
+
+
+});
+
---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function () {
+    <!-- Plugins -->
+    $(".phone_mask").mask("+7 (000) 000 00 00", {
+        placeholder: ""
+    });
+    <!-- Plugins and -->
+
+    <!-- Scroll -->
+    $('#scrollto').on('click', function() {
+        $('html,body').animate({scrollTop:$('.section_2').offset().top+"px"},{duration:1E3});
+
+    });
+    <!-- Scroll and -->
+
+    <!-- Tabs -->
+    $('.tabs-button').click(function(){
+        if($(this).is(':visible')){
+            setTimeout(function(){
+                $(".slider_1").slick('setPosition');
+                $(window).resize();
+            },0);
+        }
+    });
+    <!-- Tabs and -->
+
+    <!-- Accordion -->
+    $( function() {
+        $( ".accordion" ).accordion({
+            collapsible: true,
+
+        });
+    } );
+    <!-- Accordion and -->
+
+    <!-- Menu -->
+    $("#navToggle").click(function() {
+        $(this).toggleClass("active");
+        $(".overlay").toggleClass("open");
+        // this line ▼ prevents content scroll-behind
+        $("body").toggleClass("locked");
+        $(".text_anime").toggleClass("none");
+
+    });
+    <!-- Menu and -->
+
+
+});
